890513f2ddb27cc98098a626b00f9144c30d859b
Matthew Emmett <matthew@emmett.ca>
2009-11-24 22:52:19 -0700

    Sync.

---
+++ images/smooth.py
@@ -0,0 +1,56 @@
+"""PyWENO smooth reconstruction example."""
+
+import math
+import numpy
+import pyweno.grid
+import pyweno.weno
+
+# explicitly define the function f that we will reconstruct ...
+def f(x):
+    """Test function (quadratic)."""
+
+    return 1.0 - x + x*x
+
+# ... and it's derivative
+def fp(x):
+    """Derivative of test function."""
+
+    return -1.0 + 2.0*x
+
+# load the weno reconstructor from the cache
+k = 3
+cache = 'gridk%d.mat' % (k)
+
+grid = pyweno.grid.Grid(cache=cache)
+weno = pyweno.weno.WENO(order=k, cache=cache)
+
+# average f
+f_avg = grid.average(f)
+
+# allocate arrays for reconstruction
+f_left = numpy.zeros(grid.N)
+f_right = numpy.zeros(grid.N)
+f_gauss = numpy.zeros((grid.N, 3))
+fp_gauss = numpy.zeros((grid.N, 3))
+
+# compute smoothness indicators
+weno.smoothness(f_avg)
+
+# reconstruct!
+weno.reconstruct(f_avg, 'left', f_left)
+weno.reconstruct(f_avg, 'right', f_right)
+weno.reconstruct(f_avg, 'gauss_quad3', f_gauss)
+weno.reconstruct(f_avg, 'd|gauss_quad3', fp_gauss)
+
+# plot results
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+plt.plot(grid.x[:-1], f_left, 'or')
+plt.plot(grid.x[1:], f_right, 'ob')
+plt.savefig('smooth.png', format='png')
+
+# XXX: quadrature points too...
+
+
---
+++ sources/reference.txt
@@ -0,0 +1,38 @@
+PyWENO Reference
+================
+
+WENO class
+----------
+
+.. autoclass:: pyweno.weno.WENO
+   :members:
+
+Grid class
+----------
+
+.. autoclass:: pyweno.grid.Grid
+   :members:
+
+Stencil class
+-------------
+
+.. autoclass:: pyweno.stencil.Stencil
+   :members:
+
+Fast stencil routines
+^^^^^^^^^^^^^^^^^^^^^
+
+.. autofunction:: pyweno.stencil.reconstruction_coeffs
+
+
+Smoothness indicators
+---------------------
+
+.. automodule:: pyweno.smoothness
+   :members:
+
+
+Version information
+-------------------
+
+.. automodule:: pyweno.version
