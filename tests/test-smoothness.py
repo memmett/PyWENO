8eb64aa9cc84f2e3c6e25781de44dce7bc10199e
Matthew Emmett <matthew@emmett.ca>
2010-11-08 17:42:19 -0500

    Tidy tests.

--- tests/test-smoothness.py
+++ tests/test-smoothness.py
@@ -1,4 +1,4 @@
-"""Test smoothness indicators on an unstructured grid."""
+"""Test pyweno.smoothness module (jiang_shu smoothness indicators)."""
 
 import math
 
@@ -13,22 +13,17 @@ import pyweno.weno
 
 def test_smoothness():
 
-    x = np.array([-4.0, -3.5, -3.0, -2.5, -2.0, -1.5,
-                  -1.0, -0.8, -0.6, -0.4,
-                  -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3,
-                  0.4, 0.6, 0.8, 1.0,
-                  1.5, 2.0, 2.5, 3.0, 3.5, 4.0])
     x = np.linspace(-1.0, 1.0, 21)
     grid = pyweno.grid.Grid(boundaries=x)
 
     k = 3
     N = x.shape[0] - 1
 
-    beta = np.zeros((N,k,2*k-1,2*k-1))
+    beta = np.zeros((k,2*k-1,2*k-1))
 
     pyweno.smoothness.beta('jiang_shu', grid, k, beta)
 
-    print beta[0,:,:,:]
+    print beta
 
     # XXX: add an assert somewhere!
 
--- tests/test-weno.py
+++ tests/test-weno.py
@@ -1,5 +1,6 @@
 """Test WENO reconstruction on an unstructured grid."""
 
+import os
 import math
 
 import numpy as np
@@ -8,7 +9,15 @@ import pyweno.grid
 import pyweno.weno
 
 
-# test function and it's derivative
+######################################################################
+# test cache
+
+def test_cache():
+    raise NotImplementedError
+
+
+######################################################################
+# test by reconstructing
 
 def f(x):
     return 1.0 - x + x*x
@@ -18,17 +27,10 @@ def fp(x):
     return -1.0 + 2.0*x
 ufp = np.frompyfunc(fp, 1, 1)
 
-
-######################################################################
-
-def test_weno():
+def test_by_reconstructing_uniform():
 
     K = range(3, 5)
-    x = np.array([-4.0, -3.5, -3.0, -2.5, -2.0, -1.5,
-                  -1.0, -0.8, -0.6, -0.4,
-                  -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3,
-                  0.4, 0.6, 0.8, 1.0,
-                  1.5, 2.0, 2.5, 3.0, 3.5, 4.0])
+    x = np.linspace(0.0, 1.0)
     grid = pyweno.grid.Grid(boundaries=x)
 
     # f and f' evaluated at boundaries
@@ -40,35 +42,74 @@ def test_weno():
     fbar[0,:] = grid.average(f)
 
     for k in K:
-        weno = pyweno.weno.WENO(grid=grid, order=k)
-        weno.precompute_reconstruction('left')
-        weno.precompute_reconstruction('d|left')
 
-        # f reconstructed at boundaries
-        frcnst = np.zeros((x.size,3))
-        fprcnst = np.zeros((x.size,3))
+        for s in (0,):                  # XXX: test more s values
 
-        weno.smoothness(fbar[0,:])
-        weno.reconstruct(fbar[0,:], 'left', frcnst[:,0])
-        weno.reconstruct(fbar[0,:], 'd|left', fprcnst[:,0])
+            weno = pyweno.weno.WENO(grid=grid, order=k)
+            weno.precompute_reconstruction('left')
+            weno.precompute_reconstruction('d|left')
 
-        # assert
-        d  = fbndry[k+1:-k-1] - frcnst[k+1:-k-1,0]
-        l2 = math.sqrt(np.dot(d, d))
-        assert l2 < 1e-10, "WENO (k=%d, left) is broken" % (k)
+            # f reconstructed at boundaries
+            frcnst = np.zeros((grid.N,3))
+            fprcnst = np.zeros((grid.N,3))
 
-        d  = fpbndry[k+1:-k-1] - fprcnst[k+1:-k-1,0]
-        l2 = math.sqrt(np.dot(d, d))
-        assert l2 < 1e-10, "WENO (k=%d, d|left) is broken" % (k)
+            weno.smoothness(fbar[0,:])
+            weno.reconstruct(fbar[0,:], 'left', frcnst[:,0], s=s)
+            weno.reconstruct(fbar[0,:], 'd|left', fprcnst[:,0], s=s)
+
+            # assert
+            d  = fbndry[:-1] - frcnst[:,0]
+            l2 = math.sqrt(np.dot(d, d))
+
+            assert l2 < 1e-10, "WENO (k=%d, left) is broken" % (k)
+
+            d  = fpbndry[:-1] - fprcnst[:,0]
+            l2 = math.sqrt(np.dot(d, d))
+
+            assert l2 < 1e-10, "WENO (k=%d, d|left) is broken" % (k)
 
 
 ######################################################################
+# test coefficients
+
+def f(x):
+    return 1.0 - x + x*x
+uf = np.frompyfunc(f, 1, 1)
+
+def fp(x):
+    return -1.0 + 2.0*x
+ufp = np.frompyfunc(fp, 1, 1)
+
+
+def test_coefficients_uniform():
+
+    k = 3
+    x = np.linspace(0.0, 1.0, 6)
+    grid = pyweno.grid.Grid(boundaries=x)
+
+    fbar = grid.average(f)
+
+    weno = pyweno.weno.WENO(grid=grid, order=k)
+    weno.precompute_reconstruction('left')
+
+    frcnst = np.zeros((grid.N,3))
+
+    weno.smoothness(fbar)
+    weno.reconstruct(fbar, 'left', frcnst)
+
+    sigma_exact = np.array([[0.032533333333327259, 0.0, 0.0],
+                            [0.0133333333333289, 0.013333333333333242, 0.0],
+                            [0.0069333333333292512, 0.0069333333333333157, 0.006933333333333281],
+                            [0.0, 0.013333333333333214, 0.01333333333333335], [0.0, 0.0, 0.0]])
+    wr_exact = np.array([[[1.0, 0.0, 0.0]],
+                         [[0.2500000000011281, 0.74999999999887201, 0.0]],
+                         [[0.099999999998186304, 0.60000000000776998, 0.2999999999940437]],
+                         [[0.0, 0.49999999999929795, 0.50000000000070211]],
+                         [[0.0, 0.0, 1.0]]])
+
+    assert(np.all(sigma_exact == weno.sigma))
+    assert(np.all(wr_exact == weno.wr['left']))
 
 
 if __name__ == '__main__':
-    import cProfile as profile
-    import pstats
-
-    profile.run("test_weno()", 'weno.prof')
-    p = pstats.Stats('weno.prof')
-    p.strip_dirs().sort_stats('time', 'cum').print_stats(10)
+    test_coefficients_uniform()
